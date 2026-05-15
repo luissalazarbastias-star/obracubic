212449e1dd951f1db10fc7663b34e35e4e5f89f3
luissalazarbastias-star <luissalazarbastias@gmail.com>
2026-05-15 04:23:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="ObraCubic - Grandes Cosas Comienzan Aquí",
-    page_icon="🏗️"
+    page_icon="🏗️")
 
 st.markdown("<h1 style='text-align: center; color: #fb8c00;'>🏗️ ObraCubic</h1>", unsafe_allow_html=True)
 st.write("---")
