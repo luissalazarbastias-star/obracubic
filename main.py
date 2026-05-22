54cb305769481694ba8d1abd5545c74ae43999c0
luissalazarbastias-star <luissalazarbastias@gmail.com>
2026-05-21 21:58:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,14 @@ if option == "Panel General":
 elif option == "Cubicación":
     st.subheader("Cubicación de Hormigón", )
 
+    DOSIFICACIONES = {"H-15 (1:2:3)":{ 
+        "descripcion":"Emplantillado,Sobrecimiento",
+        "cemento_kg": 275,
+        "cemento_sacos": round(275/25),
+        "gravilla_kg": 1070, 
+        "arena_kg": 800,
+        "agua_lt": 195,
+}
     # --- 1. Partida: Emplantillado ---
     with st.expander("1. Emplantillado", expanded=False):
         emp1, emp2, emp3 = st.columns(3)
@@ -35,6 +43,15 @@ elif option == "Cubicación":
             vol_emp = (emp_largo * emp_ancho * emp_espesor) * (1 + (emp_perdida / 100))
             st.info(f"Volumen Emplantillado: {vol_emp:.2f} m3")
 
+       def calular_materiales(volumen_m3, dosificacion, desperdicio=0.05):
+           dos = DOSIFICACIONES[dosificacion]
+           vol = volumen_m3 * (1 + desperdicio)
+           return {
+        "cemento_sacos":  round(vol * dos["cemento_sacos"]),
+        "gravilla_kg":    round(vol * dos["gravilla_kg"]),
+        "arena_kg":       round(vol * dos["arena_kg"]),
+        "agua_lt":        round(vol * dos["agua_lt"]),
+    }
     # --- 2. PARTIDA: Cimiento (Pilares) ---
     with st.expander("2. Cimiento (Pilares)", expanded=False):
         c1, c2, c3 = st.columns(3)
